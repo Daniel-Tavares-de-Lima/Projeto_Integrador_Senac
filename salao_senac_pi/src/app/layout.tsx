8cdae4d01f00a226434aa96a292ce571263d1f0b
Matheus Monteiro <matheusmonteirolima1997@gmail.com>
2024-12-16 10:27:41 -0300

    Alteracao de rota

--- salao_senac_pi/src/app/layout.tsx
+++ salao_senac_pi/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import "./pages/telaServicos/servicos"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
